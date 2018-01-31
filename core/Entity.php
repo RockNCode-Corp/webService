5bfe5f205499c7cf543ec3c0b242b96eac1a62af
apomalyn <chretienxaiver42@gmail.com>
2018-01-31 14:11:37 -0500

    Factoring update and insert system Update update system, no we detect change in entity class not by setter/getter

--- core/Entity.php
+++ core/Entity.php
@@ -8,35 +8,28 @@
 
 abstract class Entity extends DataBase implements JsonSerializable {
 
+    private const UPDATE = 0;
+    private const INSERT = 1;
+
     /**
      * @var string, id of this entity
      */
     protected $id;
 
     /**
-     * @var array contains columns to update
-     * [
-     *  table => TABLE TO UPDATE
-     *  NAME_COLUMN_TO_UPDATE => NEW VALUE,
-     *  sub-table => [
-     *                  table => TABLE TO UPDATE,
-     *                  where => COLUMN TO USE FOR WHERE
-     *                  data => [
-     *                              NAME_COLUMN_TO_UPDATE => NEW VALUE,
-     *                          ]
-     *              ]
-     * ]
+     * @var array contains data actually save in the database
      */
-    protected $modify = [];
+    private $currentData = [];
 
     /**
      * Entity constructor.
      * @param $id integer
      * @param $tableName string
      */
-    public function __construct($id, $tableName) {
+    public function __construct($id) {
         $this->id = $id;
-        $this->modify['table'] = $tableName;
+
+        $this->__updateCurrentData();
     }
 
     /**
@@ -50,100 +43,108 @@ abstract class Entity extends DataBase implements JsonSerializable {
      * Update this entity
      */
     public function update(){
+        if($this->id == -1) return $this->insert();
 
-        if($this->id == -1){
-            return $this->insert();
-        }
+        $params = $this->__modify(Entity::UPDATE);
 
-        $params = $this->modify;
         $sql = 'UPDATE '.$params['table'].' SET ';
         unset($params['table']);
 
         $execArray = [];
-        $subRequests = [];
         foreach ($params as $key => $value){
-            if($key == 'sub-table'){
-                $subRequests[] = $value;
-            }else{
-                $sql .= (count($execArray) < count($params) && count($execArray) > 0) ? ', '.$key.' = :'.$value : ' '.$key.' = :'.$value;
-                $execArray[':'.$key] = $value;
-            }
+            $sql .= (count($execArray) < count($params) && count($execArray) > 0) ? ', '.$key.' = :'.$key : ' '.$key.' = :'.$key;
+            $execArray[$key] = $value;
         }
 
         $sql .= ' WHERE id = :id';
 
-        if(!empty($subRequests)){
-            foreach ($subRequests as $request){
-                $sql .= '; UPDATE '.$request['table']. ' SET ';
-                $where = ' WHERE '.$request['where'].' = :id';
-                $execTmpArray = [];
-                foreach ($request['data'] as $key => $value){
-                    $sql .= (count($execTmpArray) < count($request['data']) && count($execTmpArray) > 0) ? ', '.$key.' = :'.$value : ' '.$key.' = :'.$value;
-                    $execTmpArray[':'.$key] = $value;
-                }
-
-                $sql .= $where;
-                array_merge($execArray, $execTmpArray);
-            }
-        }
-
-        $this->modify = array('table' => $this->modify['table']);
-
-        $execArray[':id'] = $this->getId();
+        $execArray['id'] = $this->id;
 
         $this->execute($sql, $execArray, true);
+        $this->__updateCurrentData();
     }
 
     /**
      * Delete this entity
      */
     public function delete(){
-        $sql = 'DELETE FROM '.$this->modify['table'].' WHERE id = :id';
-        $this->execute($sql, array(':id' => $this->getId()), true);
+        $sql = 'DELETE FROM '.$this->currentData['table'].' WHERE id = :id';
+        $this->execute($sql, array('id' => $this->id), true);
     }
 
     /**
      * Insert this entity on database
      */
-    public function insert(){
-        $params = $this->modify;
+    private function insert(){
+        $params = $this->__modify(Entity::INSERT);
         $sql = 'INSERT INTO '.$params['table'].' (';
         $values = ") VALUE (";
         unset($params['table']);
 
         $execArray = [];
-        $subRequests = [];
         foreach ($params as $key => $value){
-            if($key == 'sub-table'){
-                $subRequests[] = $value;
-            }else{
-                $sql .= (count($execArray) < count($params) && count($execArray) > 0) ? ', '.$key: ' '.$key;
-                $values .= (count($execArray) < count($params) && count($execArray) > 0) ? ', :'.$key: ' :'.$key;
-                $execArray[':'.$key] = $value;
-            }
+            $sql .= (count($execArray) < count($params) && count($execArray) > 0) ? ', '.$key: ' '.$key;
+            $values .= (count($execArray) < count($params) && count($execArray) > 0) ? ', :'.$key: ' :'.$key;
+            $execArray[$key] = $value;
         }
 
         $sql .= $values.")";
 
-        if(!empty($subRequests)){
-            foreach ($subRequests as $request){
-                $sql .= '; INSERT INTO '.$request['table']. ' ( ';
-                $values = ') VALUE (';
+        $this->id = $this->execute($sql, $execArray, true, true)['id'];
+        $this->__updateCurrentData();
+    }
 
-                $execTmpArray = [];
-                foreach ($request['data'] as $key => $value){
-                    $sql .= (count($execTmpArray) < count($request['data']) && count($execTmpArray) > 0) ? ', '.$key: ' '.$key;
-                    $values .= (count($execTmpArray) < count($request['data']) && count($execTmpArray) > 0) ? ', :'.$key: ' :'.$key;
-                    $execTmpArray[':'.$key] = $value;
+    /**
+     * Calculate the diffence between the version in database and this version
+     * @param int $type UPDATE or INSERT
+     * @return array change to save in database
+     */
+    private function __modify(int $type) : array {
+        $return = [];
+        $reflection = new ReflectionClass($this);
+        $return['table'] = DocParser::getValueOf('table', $reflection->getDocComment());
+
+        $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE);
+        foreach ($properties as $property){
+            if($property->getName() != 'id' || $property->getName() != 'currentData'){
+                $doc = $property->getDocComment();
+
+                $name = DocParser::getValueOf('name', $doc);
+                $property->setAccessible(1);
+                $value = $property->getValue($this);
+                $property->setAccessible(0);
+
+                if(!empty(DocParser::getValueOf("sub", $doc)) && empty(DocParser::getValueOf('foreign', $doc))){
+                    if($type == Entity::UPDATE) $value->update();
+                    else if($type == Entity::INSERT) $value->insert();
+                }else if(!empty($name) && $this->currentData[$name] != $value){
+                    $return[$name] = $value;
                 }
-
-                $sql .= $values.")";
-                array_merge($execArray, $execTmpArray);
             }
         }
+        return $return;
+    }
 
-        $this->modify = array('table' => $this->modify['table']);
+    /**
+     * Synchronise the version in database and this version. This function is use for calculate the difference during a update
+     */
+    private function __updateCurrentData(){
+        $reflection = new ReflectionClass($this);
+        $this->currentData['table'] = DocParser::getValueOf('table', $reflection->getDocComment());
+        $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE);
+        foreach ($properties as $property){
+            if($property->getName() != 'id'){
+                $doc = $property->getDocComment();
 
-        $this->id = $this->execute($sql, $execArray, true, true)['id'];
+                $name = DocParser::getValueOf('name', $doc);
+                $property->setAccessible(1);
+                $value = $property->getValue($this);
+                $property->setAccessible(0);
+
+                if(!empty($name) && empty(DocParser::getValueOf('sub', $doc)) && empty(DocParser::getValueOf('foreign', $doc))){
+                    $this->currentData[$name] = $value;
+                }
+            }
+        }
     }
 }
